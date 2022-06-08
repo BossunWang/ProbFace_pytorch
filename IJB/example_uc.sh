f93fcd09c2811446705b0640c4fe07f8ca69c791
Bossun Wang <vvmodouco@gmail.com>
2022-06-08 17:20:59 +0800

    change IJBC script to eval prob_face_ir_101_masked

--- IJB/example_uc.sh
+++ IJB/example_uc.sh
@@ -11,7 +11,7 @@
 CUDA_VISIBLE_DEVICES=1 python -u IJB_11_uc.py \
   --model_type IR_101 \
   --model_path ../pretrained/adaface_ir101_webface12m.ckpt \
-  --uc_model_path ../prob_face_ir_101_masked/Epoch_0.pt \
+  --uc_model_path ../prob_face_ir_101_masked/Epoch_0_batch_59999.pt \
   --gpu 0 \
   --image-path ../../face_dataset/IJB-C/IJB/IJB_release/IJBC \
   --job ir101_adaface_uc_masked \
