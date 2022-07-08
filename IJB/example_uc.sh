5bf00b03fd54ea99040179bfead31a0fc0e21a4e
Bossun Wang <vvmodouco@gmail.com>
2022-07-08 12:09:33 +0800

    eval IJBC for data aug version

--- IJB/example_uc.sh
+++ IJB/example_uc.sh
@@ -8,13 +8,23 @@
 #  --result-dir "ir101_adaface_uc" \
 #2>&1 | tee ir101_adaface_uc.log
 
+#CUDA_VISIBLE_DEVICES=1 python -u IJB_11_uc.py \
+#  --model_type IR_101 \
+#  --model_path ../pretrained/adaface_ir101_webface12m.ckpt \
+#  --uc_model_path ../prob_face_ir_101_masked/Epoch_0_batch_59999.pt \
+#  --gpu 0 \
+#  --image-path ../../face_dataset/IJB-C/IJB/IJB_release/IJBC \
+#  --job ir101_adaface_uc_masked \
+#  --result-dir "ir101_adaface_uc_masked" \
+#  --skip_extraction \
+#2>&1 | tee ir101_adaface_uc_masked.log
+
 CUDA_VISIBLE_DEVICES=1 python -u IJB_11_uc.py \
   --model_type IR_101 \
   --model_path ../pretrained/adaface_ir101_webface12m.ckpt \
-  --uc_model_path ../prob_face_ir_101_masked/Epoch_0_batch_59999.pt \
+  --uc_model_path ../prob_face_ir_101_masked_data_aug/Epoch_0_batch_49999.pt \
   --gpu 0 \
   --image-path ../../face_dataset/IJB-C/IJB/IJB_release/IJBC \
   --job ir101_adaface_uc_masked \
-  --result-dir "ir101_adaface_uc_masked" \
-  --skip_extraction \
-2>&1 | tee ir101_adaface_uc_masked.log
+  --result-dir "ir101_adaface_uc_masked_data_aug" \
+2>&1 | tee ir101_adaface_uc_masked_data_aug.log
